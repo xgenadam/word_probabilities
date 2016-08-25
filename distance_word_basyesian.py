d9277c30f2f5d4319773e9d2caef91b9532997bf
Adam Merchant <xgenadam@mmachine.home>
2016-08-25 22:58:02 +0100

    initial commit

---
+++ distance_word_basyesian.py
@@ -0,0 +1,68 @@
+# ------------------------------------------------------------------
+
+#
+#   Bayes Optimal Classifier
+#
+#   In this quiz we will compute the optimal label for a second missing word in a row
+#   based on the possible words that could be in the first blank
+#
+#   Finish the procedurce, LaterWords(), below
+#
+#   You may want to import your code from the previous programming exercise!
+#
+
+sample_memo = '''
+Milt, we're gonna need to go ahead and move you downstairs into storage B. We have some new people coming in, and we need all the space we can get. So if you could just go ahead and pack up your stuff and move it down there, that would be terrific, OK?
+Oh, and remember: next Friday... is Hawaiian shirt day. So, you know, if you want to, go ahead and wear a Hawaiian shirt and jeans.
+Oh, oh, and I almost forgot. Ahh, I'm also gonna need you to go ahead and come in on Sunday, too...
+Hello Peter, whats happening? Ummm, I'm gonna need you to go ahead and come in tomorrow. So if you could be here around 9 that would be great, mmmk... oh oh! and I almost forgot ahh, I'm also gonna need you to go ahead and come in on Sunday too, kay. We ahh lost some people this week and ah, we sorta need to play catch up.
+'''
+
+corrupted_memo = '''
+Yeah, I'm gonna --- you to go ahead --- --- complain about this. Oh, and if you could --- --- and sit at the kids' table, that'd be ---
+'''
+
+words_to_guess = ["ahead", "could"]
+
+
+def LaterWords(sample, word, distance):
+    from max_likelihood import next_word_probabilities_for_given_words
+    '''
+    @param sample: a sample of text to draw from
+    @param word: a word occuring before a corrupted sequence
+    @param distance: how many words later to estimate (i.e. 1 for the next word, 2 for the word after that)
+    @returns: a single word which is the most likely possibility
+    '''
+
+    # TODO: Given a word, collect the relative probabilities of possible following words
+    # TODO: Repeat the above process--for each distance beyond 1, evaluate the words that
+    # might come after each word, and combine them weighting by relative probability
+    # into an estimate of what might appear next.
+
+    word_succession_probabilities = next_word_probabilities_for_given_words(sample)
+
+    cumulative_succession_probabilites = [word_succession_probabilities]
+    for x in xrange(1, distance + 1):
+        print x
+        next_prob = {preceeding_word: {succeeding_word: 0.0 for succeeding_word in word_succession_probabilities.keys()}
+                     for preceeding_word in word_succession_probabilities.keys()}
+        for preceeding_word, successive_word_dict in cumulative_succession_probabilites[x-1].items():
+            for successive_word, probability_of_successive_word in successive_word_dict.items():
+                absolute_prob = word_succession_probabilities[preceeding_word][successive_word]
+                if absolute_prob > 0:
+                    try:
+                        if x == 2:
+                            print successive_word, probability_of_successive_word, preceeding_word, probability_of_successive_word * word_succession_probabilities[preceeding_word][successive_word]
+                        next_prob[preceeding_word][successive_word] += probability_of_successive_word * word_succession_probabilities[preceeding_word][successive_word]
+                    except Exception as e:
+                        import pdb; pdb.set_trace()
+
+        cumulative_succession_probabilites.append(next_prob)
+    # from @sample. You may want to import your code from the maximum likelihood exercise.
+    # for w, p in cumulative_succession_probabilites[-1][word].items():
+    #     print w, p
+    most_likely_word = sorted(cumulative_succession_probabilites[-1][word].items(), key=lambda t: -t[1])[0][0]
+    return most_likely_word
+
+
+print LaterWords(sample_memo, "ahead", 2)
---
+++ max_likelihood.py
@@ -0,0 +1,67 @@
+sample_memo = '''
+Milt, we're gonna need to go ahead and move you downstairs into storage B. We have some new people coming in, and we need all the space we can get. So if you could just go ahead and pack up your stuff and move it down there, that would be terrific, OK?
+Oh, and remember: next Friday... is Hawaiian shirt day. So, you know, if you want to, go ahead and wear a Hawaiian shirt and jeans.
+Oh, oh, and I almost forgot. Ahh, I'm also gonna need you to go ahead and come in on Sunday, too...
+Hello Peter, whats happening? Ummm, I'm gonna need you to go ahead and come in tomorrow. So if you could be here around 9 that would be great, mmmk... oh oh! and I almost forgot ahh, I'm also gonna need you to go ahead and come in on Sunday too, kay. We ahh lost some people this week and ah, we sorta need to play catch up.
+'''
+
+#
+#   Maximum Likelihood Hypothesis
+#
+#
+#   In this quiz we will find the maximum likelihood word based on the preceding word
+#
+#   Fill in the NextWordProbability procedure so that it takes in sample text and a word,
+#   and returns a dictionary with keys the set of words that come after, whose values are
+#   the number of times the key comes after that word.
+#
+#   Just use .split() to split the sample_memo text into words separated by spaces.
+
+
+def format_text(text, remove_chars=('.', ',', '\'', '\"', '!', '?', '\n'), replacement_char=''):
+    new_text = text.lower()
+    for remove_char in remove_chars:
+        new_text = new_text.replace(remove_char, replacement_char)
+    return new_text
+
+
+def offset_zip(l1, l2, offset=0):
+    from itertools import chain
+    return zip(l1, chain(l2[offset:], l2[:offset]))
+
+
+def most_likely_next_word(sampletext, preceding_word):
+    word_list = format_text(sampletext).split()
+    word_dict = {succeding_word: {other_word:0 for other_word in word_list} for succeding_word in set(word_list)}
+    for preceding_word_, successive_word in list(offset_zip(word_list, word_list, 1))[:-1]:
+        word_dict[preceding_word_][successive_word] +=1
+
+    return word_dict[preceding_word]
+
+
+def num_next_words(sample_text):
+    formated_text = format_text(sample_text).split()
+    word_dict = {preceeding_word: {successive_word: 0 for successive_word in set(formated_text)} for preceeding_word in set(formated_text)}
+
+    for preceeding_word, succeeding_word in offset_zip(formated_text, formated_text, 1):
+        word_dict[preceeding_word][succeeding_word] += 1
+
+    return word_dict
+
+
+def next_word_probabilities_for_given_words(sampletext):
+    num_next_word_dict = num_next_words(sampletext)
+    probability_dict = {preceeding_word: {} for preceeding_word in set(format_text(sampletext).split())}
+    for preceeding_word, successive_word_dict in num_next_word_dict.items():
+        total_num_succeeding_words = sum(successive_word_dict.values())
+        for successive_word, successive_word_count in successive_word_dict.items():
+            try:
+                probability_dict[preceeding_word][successive_word] = float(successive_word_count)/float(total_num_succeeding_words)
+            except Exception as e:
+                import pdb; pdb.set_trace()
+
+    # for preceeding_word, successive_word_dict in probability_dict.items():
+    #     if sum(successive_word_dict.values()) != 1.0:
+    #         print preceeding_word, sum(successive_word_dict.values())
+
+    return probability_dict
